a12e36f5bd28094054aabcfcfec765b4e4089fdc
ianmtzb <ianmtzb@gmail.com>
2025-04-03 17:17:10 +0000

    Ajuste fundo e botão tela inicial

--- lib/src/initial_screen.dart
+++ lib/src/initial_screen.dart
@@ -15,10 +15,11 @@ class _InitialScreenState extends State<InitialScreen> {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: <Color>[Color(0xFFB6335F), Color(0xFFF1867C)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+          image: DecorationImage(
+            image: AssetImage("assets/FundoBoasVindasComCor.png"),
+            fit: BoxFit.cover,
+            alignment: Alignment.centerLeft,
+            scale: 2,
           ),
         ),
         child: Column(
@@ -40,36 +41,39 @@ class _InitialScreenState extends State<InitialScreen> {
             ),
 
             Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF9E2244),
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                  minimumSize: Size(140, 70),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(width: 10),
-                    Text(
-                      'Começar',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
+              padding: EdgeInsets.only(bottom: 30),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF9E2244),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
                     ),
-                    Icon(Icons.arrow_forward, color: Colors.white, size: 18),
-                  ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Começar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Icon(Icons.arrow_forward, color: Colors.white, size: 18),
+                    ],
+                  ),
                 ),
               ),
             ),
